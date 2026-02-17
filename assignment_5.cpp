42494b402a1aa2ea619f3abbf1ee6b0795aa4cbc
computer <vyaas188@VY028-02.local>
2026-02-17 11:56:54 +0530

    Changes made but without pointer

--- assignment_5.cpp
+++ assignment_5.cpp
@@ -6,6 +6,34 @@ class Shape
     public:
     virtual void drawing()
     {
-        
+        cout<<"Here is a drawing"<<endl;
     }
 };
+
+class Circle:public Shape
+{
+    public:
+    void drawing()
+    {
+        cout<<"This is a Circle"<<endl;
+    }
+};
+
+class Rectangle:public Shape
+{
+    public:
+    void drawing()
+    {
+        cout<<"This is a Rectangle"<<endl;
+    }
+};
+
+int main()
+{
+    class Shape s;
+    s.drawing();
+    class Circle c;
+    c.drawing();
+    class Rectangle r;
+    r.drawing();
+}
